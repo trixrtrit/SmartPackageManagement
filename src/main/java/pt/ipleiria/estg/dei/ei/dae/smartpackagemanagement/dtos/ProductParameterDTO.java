931bfa569ad473952190b19b024d50a7747e1d81
João Silva <2211429@my.ipleiria.pt>
2023-12-27 18:59:10 +0000

    Rework ProductParameterDTO to include Id and SensorTypeDTO

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/dtos/ProductParameterDTO.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/dtos/ProductParameterDTO.java
@@ -1,10 +1,13 @@
 package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos;
 
 public class ProductParameterDTO {
+    private Long id;
 
     private Long productId;
 
     private Long sensorTypeId;
+
+    private SensorTypeDTO sensorType;
 
     private float minValue;
 
@@ -13,13 +16,31 @@ public class ProductParameterDTO {
     public ProductParameterDTO() {
     }
 
-    public ProductParameterDTO(Long productId, Long sensorTypeId, float minValue, float maxValue) {
+    public ProductParameterDTO(Long id, Long productId, Long sensorTypeId, float minValue, float maxValue) {
+        this.id = id;
         this.productId = productId;
         this.sensorTypeId = sensorTypeId;
         this.minValue = minValue;
         this.maxValue = maxValue;
+        this.sensorType = null;
     }
 
+    public ProductParameterDTO(Long id, Long productId, Long sensorTypeId, float minValue, float maxValue, SensorTypeDTO sensorType) {
+        this.id = id;
+        this.productId = productId;
+        this.sensorTypeId = sensorTypeId;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.sensorType = sensorType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Long getProductId() {
         return productId;
@@ -51,5 +72,9 @@ public class ProductParameterDTO {
 
     public void setMaxValue(float maxValue) {
         this.maxValue = maxValue;
+    }
+
+    public SensorTypeDTO getSensorType() {
+        return sensorType;
     }
 }
