c5a02e02ace2253322eecdf77ee92f742e9e21e9
João Silva <2211429@my.ipleiria.pt>
2024-01-08 20:55:18 +0000

    Refactor OrderDTO

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/dtos/OrderDTO.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/dtos/OrderDTO.java
@@ -1,24 +1,39 @@
 package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos;
 
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.entities.Customer;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.entities.OrderItem;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.enums.OrderStatus;
+
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class OrderDTO implements Serializable {
     private Long id;
     private String address;
     private double totalPrice;
     private Date date;
-    private String status;
+    private OrderStatus status;
+    private CustomerDTO customer;
+    private List<OrderItemDTO> orderItems;
 
     public OrderDTO() {
+        orderItems = new ArrayList<>();
     }
 
-    public OrderDTO(Long id, String address, double totalPrice, Date date, String status) {
+    public OrderDTO(Long id, String address, double totalPrice, Date date, OrderStatus status, CustomerDTO customer) {
+        this(id, address, totalPrice, date, status, customer, new ArrayList<>());
+    }
+
+    public OrderDTO(Long id, String address, double totalPrice, Date date, OrderStatus status, CustomerDTO customer, List<OrderItemDTO> orderItems) {
         this.id = id;
         this.address = address;
         this.totalPrice = totalPrice;
         this.date = date;
         this.status = status;
+        this.customer = customer;
+        this.orderItems = orderItems;
     }
 
     public Long getId() {
@@ -53,11 +68,27 @@ public class OrderDTO implements Serializable {
         this.date = date;
     }
 
-    public String getStatus() {
+    public OrderStatus getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(OrderStatus status) {
         this.status = status;
+    }
+
+    public CustomerDTO getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerDTO customer) {
+        this.customer = customer;
+    }
+
+    public List<OrderItemDTO> getOrderItems() {
+        return orderItems;
+    }
+
+    public void setOrderItems(List<OrderItemDTO> orderItems) {
+        this.orderItems = orderItems;
     }
 }
