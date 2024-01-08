e99c2ca6900f96c47a2c7adf25fc35dad1caf1ba
João Silva <2211429@my.ipleiria.pt>
2024-01-08 20:57:02 +0000

    Add OrderStatus enum

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.enums;
+
+public enum OrderStatus {
+    Pending,
+    Accepted,
+    Rejected,
+    Packaged,
+    OutForDelivery,
+    Delivered,
+    Complained,
+    ComplainAcknowledged
+}
