af7169617f4e71aac66d21bb6a652458d267fbe1
João Silva <2211429@my.ipleiria.pt>
2024-01-15 23:39:37 +0000

    Add custom serialization methods

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/serialization/EnumCustomSerialization.java
@@ -0,0 +1,20 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.serialization;
+
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.io.IOException;
+
+@JsonSerialize
+public interface EnumCustomSerialization {
+    String toString();
+}
+
+
