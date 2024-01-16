bdb944774fe239a51c8f3572b0a9c5803621e9d9
Tiago Ferreira <tiagoarf420@gmail.com>
2024-01-16 22:01:48 +0000

    order logs fixes

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/assemblers/OrderLogAssembler.java
@@ -0,0 +1,21 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.assemblers;
+
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos.OrderLogDTO;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.entities.OrderLog;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderLogAssembler {
+    public static OrderLogDTO from(OrderLog orderLog){
+        return new OrderLogDTO(
+                orderLog.getLogEntry(),
+                orderLog.getTimestamp(),
+                orderLog.getOrder().getId()
+        );
+    }
+
+    public static List<OrderLogDTO> from(List<OrderLog> orderLogs) {
+        return orderLogs.stream().map(OrderLogAssembler::from).collect(Collectors.toList());
+    }
+}
