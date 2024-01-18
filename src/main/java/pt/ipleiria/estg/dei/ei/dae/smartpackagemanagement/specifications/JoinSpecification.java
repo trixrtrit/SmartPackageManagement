29fbe1720bb8b85a87100156f2f7c7c67865ab4a
João Silva <2211429@my.ipleiria.pt>
2024-01-18 15:34:15 +0000

    Add EnumSpecification and JoinSpecification

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/specifications/JoinSpecification.java
@@ -0,0 +1,29 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.specifications;
+
+import jakarta.persistence.criteria.*;
+
+public class JoinSpecification<T, J> implements Specification<T> {
+    private String joinField;
+    private String fieldOfJoinedEntity;
+    private Object value;
+    private String operation;
+
+    public JoinSpecification(String joinField, String fieldOfJoinedEntity, Object value, String operation) {
+        this.joinField = joinField;
+        this.fieldOfJoinedEntity = fieldOfJoinedEntity;
+        this.value = value;
+        this.operation = operation;
+    }
+
+    @Override
+    public Predicate toPredicate(Root<T> root, CriteriaBuilder criteriaBuilder) {
+        Join<T, J> join = root.join(joinField);
+        Path<J> fieldPath = join.get(fieldOfJoinedEntity);
+
+        if ("notEqual".equalsIgnoreCase(operation)) {
+            return criteriaBuilder.notEqual(fieldPath, value);
+        } else { // Default to "equal"
+            return criteriaBuilder.equal(fieldPath, value);
+        }
+    }
+}
