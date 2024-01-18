da2b062831c40999d7357e94cb55dde69cdf0a77
João Silva <2211429@my.ipleiria.pt>
2024-01-18 15:42:14 +0000

    Add PredicateUtils

---
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/specifications/PredicateUtils.java
@@ -0,0 +1,24 @@
+package pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.specifications;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+
+public class PredicateUtils {
+    public static <T, E extends Enum<E>> Predicate handleEnumPredicate(
+            String fieldName, String enumValue,  String operation, CriteriaBuilder builder, Root<T> root) {
+        try {
+            String packageName = "pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.enums";
+            Class<E> enumClass = (Class<E>) Class.forName(packageName + "." + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1));
+            E enumConstant = Enum.valueOf(enumClass, enumValue.toUpperCase());
+
+            if ("notEqual".equalsIgnoreCase(operation)) {
+                return builder.notEqual(root.get(fieldName), enumConstant);
+            } else { // Default to "equal"
+                return builder.equal(root.get(fieldName), enumConstant);
+            }
+        } catch (ClassNotFoundException | IllegalArgumentException e) {
+            throw new RuntimeException("Enum handling error: " + e.getMessage(), e);
+        }
+    }
+}
