1d34d61c0e22aa6e5758f3576b9d02c3e668710a
Tiago Ferreira <tiagoarf420@gmail.com>
2024-01-20 22:24:51 +0000

    notifications for log ops ws

--- src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/ws/LogisticsOperatorService.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/dae/smartpackagemanagement/ws/LogisticsOperatorService.java
@@ -9,11 +9,14 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.assemblers.LogisticsOperatorAssembler;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.assemblers.NotificationAssembler;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos.EmailDTO;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos.LogisticsOperatorDTO;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos.ManufacturerDTO;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.dtos.NotificationDTO;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.ejbs.EmailBean;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.ejbs.LogisticsOperatorBean;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.ejbs.NotificationBean;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.entities.LogisticsOperator;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.exceptions.MyConstraintViolationException;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.exceptions.MyEntityExistsException;
@@ -22,7 +25,9 @@ import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.pagination.PaginationM
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.pagination.PaginationResponse;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.security.Authenticated;
 import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.specifications.GenericFilterMapBuilder;
+import pt.ipleiria.estg.dei.ei.dae.smartpackagemanagement.utils.DateUtil;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,6 +38,8 @@ public class LogisticsOperatorService {
 
     @EJB
     private LogisticsOperatorBean logisticsOperatorBean;
+    @EJB
+    private NotificationBean notificationBean;
     @EJB
     private EmailBean emailBean;
     @Context
@@ -82,6 +89,40 @@ public class LogisticsOperatorService {
         return Response.status(Response.Status.NOT_FOUND)
                 .entity("ERROR_FINDING_LOGISTICS_OPERATOR")
                 .build();
+    }
+
+    @GET
+    @Path("{username}/notifications")
+    @Authenticated
+    @RolesAllowed({"LogisticsOperator"})
+    public Response getNotifications(
+            @PathParam("username") String username,
+            @QueryParam("text") String text,
+            @QueryParam("startTime") Long startTime,
+            @QueryParam("endTime") Long endTime,
+            @DefaultValue("1") @QueryParam("page") int page,
+            @DefaultValue("10") @QueryParam("pageSize") int pageSize) {
+        var principal = securityContext.getUserPrincipal();
+
+        if (securityContext.isUserInRole("LogisticsOperator") && !principal.getName().equals(username)) {
+            return Response.status(Response.Status.FORBIDDEN).build();
+        }
+
+        Date[] dates = DateUtil.parseDates(startTime, endTime);
+        Date startDate = dates[0];
+        Date endDate = dates[1];
+
+        Map<String, String> filterMap = new HashMap<>();
+        GenericFilterMapBuilder.addToFilterMap(text, filterMap, "text", "");
+        GenericFilterMapBuilder.addToFilterMap(startDate, filterMap, "timestamp", "gte");
+        GenericFilterMapBuilder.addToFilterMap(endDate, filterMap, "timestamp", "lte");
+        var notifications = notificationBean.getNotifications(filterMap, page, pageSize);
+        var dtos = NotificationAssembler.from(notifications);
+        long totalItems = notificationBean.getNotificationsCount(filterMap);
+        long totalPages = (totalItems + pageSize - 1) / pageSize;
+        PaginationMetadata paginationMetadata = new PaginationMetadata(page, pageSize, totalItems, totalPages, dtos.size());
+        PaginationResponse<NotificationDTO> paginationResponse = new PaginationResponse<>(dtos, paginationMetadata);
+        return Response.ok(paginationResponse).build();
     }
 
     @POST
